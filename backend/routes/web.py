4d1aff8ced18d224572a75311ac0251ae4f5e2c4
Angel <80108054+Angel0503@users.noreply.github.com>
2026-03-30 15:41:16 +0200

    Merge pull request #1 from MathisHeriveau/setup_routes
    Setup api and app routes

--- backend/routes/web.py
+++ backend/routes/web.py
@@ -4,10 +4,37 @@ from flask import Blueprint, render_template
 
 from ..db import DATABASE_LABEL
 
+from wrapper import login_required
 
 web_bp = Blueprint("web", __name__)
 
-
 @web_bp.get("/")
-def index():
-    return render_template("index.html", db_name=DATABASE_LABEL)
+def home():
+    """
+    Home page
+    """
+    # If user in session, render home page
+    # else, redirect to login page
+    pass
+
+@web_bp.get("/register")
+def register():
+    """
+    Register page
+    """
+    pass
+
+@web_bp.get("/login")
+def login():
+    """
+    Login page
+    """
+    pass
+
+@web_bp.get("/account")
+@login_required
+def account():
+    """
+    Account page
+    """
+    pass
