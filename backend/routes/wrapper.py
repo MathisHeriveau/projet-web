d76fbc10ede4a2d4b521f50e34e766c3111fa84c
Angel <80108054+Angel0503@users.noreply.github.com>
2026-03-30 17:34:15 +0200

    Merge pull request #3 from MathisHeriveau/api_logins
    Api register, login and logout

--- backend/routes/wrapper.py
+++ backend/routes/wrapper.py
@@ -1,8 +1,20 @@
+from flask import session, g
+from backend.models import User
+
 def login_required(f):
     """
         Login required wrapper
     """
     def wrapper(*args, **kwargs):
-        pass
+        if "user" not in session:
+            return {"error": "you are not logged in"}, 401
+
+        user = User.get_by_username(session["user"])
+        if user is None:
+            session.clear()
+            return {"error": "invalid user"}, 401
+
+        g.user = user
+        return f(*args, **kwargs)
     wrapper.__name__ = f.__name__
     return wrapper
