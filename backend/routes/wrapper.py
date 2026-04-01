f76fcfeae97b87078577bf034cdcf210d2976a2d
Angel0503 <angel.ag64100@gmail.com>
2026-04-01 16:40:17 +0200

    HTTPStatus added to wrappers

--- backend/routes/wrapper.py
+++ backend/routes/wrapper.py
@@ -1,5 +1,6 @@
 from flask import session, g
 from backend.models import User
+from backend.enums.http_status import HTTPStatus
 
 def login_required(f):
     """
@@ -7,12 +8,12 @@ def login_required(f):
     """
     def wrapper(*args, **kwargs):
         if "user" not in session:
-            return {"error": "you are not logged in"}, 401
+            return {"error": "you are not logged in"}, HTTPStatus.UNAUTHORIZED.value
 
         user = User.get_by_username(session["user"])
         if user is None:
             session.clear()
-            return {"error": "invalid user"}, 401
+            return {"error": "invalid user"}, HTTPStatus.UNAUTHORIZED.value
 
         g.user = user
         return f(*args, **kwargs)
